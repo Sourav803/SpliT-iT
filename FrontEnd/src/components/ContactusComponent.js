b152227254844e2af486eb8e9f292d56e0b3078e
Sourav803 <108151952+Sourav803@users.noreply.github.com>
2022-07-09 21:02:32 +0530

    updated contact components

--- FrontEnd/src/components/ContactusComponent.js
+++ FrontEnd/src/components/ContactusComponent.js
@@ -14,15 +14,15 @@ class Contactus extends Component{
           </div>
           <div className="row">
           <p>
-            Akash Anand<br/>
+            Sourav Sinha<br/>
           B.Tech <br/>
           Electronics and Communication Engineering<br/>
-          2018-22<br/>
-          <i className="fa fa-university"></i>: IIT(ISM), Dhanbad<br/> 
+          2019-23<br/>
+          <i className="fa fa-university"></i>: BIT Mesra, Ranchi<br/> 
           <i className="fa fa-phone fa-lg"></i>: +91 1234 5678<br />
 		              
-		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                         anandakash375@gmail.com</a>
+		              <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:souravhzb3@gmail.com">
+                         souravhzb3@gmail.com</a>
           </p>
           {
             window.screen.width>=700&&<>
