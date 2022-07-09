8ec046e949cd9135de9b8206f64401d7d304c736
Sourav803 <108151952+Sourav803@users.noreply.github.com>
2022-07-09 20:59:11 +0530

    updated icons

--- FrontEnd/src/components/FooterComponent.js
+++ FrontEnd/src/components/FooterComponent.js
@@ -29,11 +29,11 @@ function Footer(props) {
                 <div className="col-12 col-sm-4 align-self-center">
                     <div className="text-center">
                         <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com/profile.php?id=100009061662539"><i className="fa fa-facebook"></i></a>
-                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/akash-anand-690675193/"><i className="fa fa-linkedin"></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="/"><i className="fa fa-facebook"></i></a>
+                        <a className="btn btn-social-icon btn-linkedin" href="/"><i className="fa fa-linkedin"></i></a>
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
                         <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                        <a className="btn btn-social-icon" href="mailto:anandakash375@gmail.com"><i className="fa fa-envelope-o"></i></a>
+                        <a className="btn btn-social-icon" href="mailto:souravhzb3@gmail.com"><i className="fa fa-envelope-o"></i></a>
                     </div>
                 </div>
             </div>
